7399d0659e079775afc9981e7aea49a1387842de
Zach Clayburn <zachclayburn@gmail.com>
2021-10-07 15:48:03 -0600

    generate_asm now spits out the minimal assembly

--- src/assembly/mod.rs
+++ src/assembly/mod.rs
@@ -3,5 +3,31 @@ use crate::parser::Program;
 type Result<T> = std::result::Result<T, String>;
 
 pub fn generate_asm(program: Program) -> Result<String> {
-    Ok("".to_string())
+    let mut lines = vec!["global main".to_string()];
+    lines.push("            section .text".into());
+    lines.push("main:".into());
+    lines.push("            mov     rax, 60".into());
+    lines.push("            xor     rdi, rdi".into());
+    lines.push("            syscall".into());
+    Ok(lines.join("\n"))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use indoc::indoc;
+
+    #[test]
+    fn can_process_minimal_program() {
+        let program = Program::new("program".to_string());
+        let asm = generate_asm(program);
+        let expected = indoc! {"
+            global main
+                        section .text
+            main:
+                        mov     rax, 60
+                        xor     rdi, rdi
+                        syscall"};
+        assert_eq!(asm.unwrap(), expected);
+    }
 }
